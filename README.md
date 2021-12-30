96f20e57ff096090b301425d02d7aacd67e8592b
ullas <ullaskunder3@gmail.com>
2021-12-30 23:05:15 +0530

    cli version 1.1.1-1

---
+++ README.md
@@ -0,0 +1,42 @@
+# Tweety generator
+
+Boilerplate, It's about doing things right the first time, so you do not waste time
+
+![tweetyimg](images/tweety.png)
+
+![tweety-create](https://img.shields.io/badge/%40tweety--create-generator-green)
+![tweety-create](https://img.shields.io/static/v1?label=cli&message=no-more-time-waste&color=red?)
+![nodejs](https://img.shields.io/badge/%40JS-nodejs-yellow)
+![vanilla](https://img.shields.io/static/v1?message=JavaScript&logo=javascript&labelColor=fdfdfa&color=c2a40f&logoColor=yellow&label=%20)
+
+![ullaskunder3](https://img.shields.io/twitter/follow/ullaskunder3?label=%40ullaskunder&style=social)
+![ullaskunder3](https://img.shields.io/github/followers/ullaskunder3?label=ullaskunder3&style=social)
+---
+
+## Generate Initial Boilerplate
+
+Cli tool `tweety-create` is a no build configuration with minimal initial project starter code creation tool for students
+
+## Installation
+
+Before installing the package please `download and install` Node.js.
+
+Node.js 6 or `higher` is required.
+
+(The recommended way):
+
+Install `tweety-create` globally on your machine by using `-g`
+
+```js
+npm install -g tweety-create
+```
+
+There you gooo... __Thats it__
+
+## Quick Start
+
+Run Script
+
+```bash
+tweety-create
+```
---
+++ main.js
@@ -0,0 +1,123 @@
+#!/usr/bin/env node
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   /!\ DO NOT MODIFY THIS FILE /!\
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/**
+ * Copyright (c) 2021-present, Ullas Kunder, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   /!\ DO NOT MODIFY THIS FILE /!\
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+'use strict';
+
+process.on('unhandledRejection', err => {
+    throw err;
+});
+
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { colorRef: pixidust } = require('./bin/utility')
+
+const directory = path.join(__dirname, 'templates/jhcTemplate');
+const currentRoot = path.basename(path.resolve(process.cwd()));
+const htmlfile = path.join(process.cwd(), 'index.html')
+
+let projectTitle = 'My Project';
+
+var realLine = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function asyncCreateFiles(callback) {
+
+    fs.readdir(directory, (err, files) => {
+        console.log('creating required files ...in a moment ♟');
+
+        if (err) {
+            return console.log('Unable to scan project dirctory' + err);
+        }
+        files.forEach((file) => {
+            var readStream = fs.createReadStream(directory + '\\' + file, 'utf-8');
+            var writeStream = fs.createWriteStream(file);
+            readStream.pipe(writeStream)
+        })
+    })
+    setTimeout(() => {
+        callback()
+    }, 1000)
+}
+
+function alterConetent() {
+
+    let style = `${pixidust.FgMagenta}style${pixidust.resetBCol}`;
+    let index = `${pixidust.FgMagenta}index${pixidust.resetBCol}`;
+    let script = `${pixidust.FgMagenta}script${pixidust.resetBCol}`;
+    let wish = `${pixidust.FgRed}HAPPY CODING... ( ｡ ◕ ‿ ◕ ｡ )${pixidust.resetBCol}`;
+    let currentWorkDir = `${pixidust.BgMagenta} ${currentRoot} ${pixidust.resetBCol}`
+
+    console.log('Applying regression...✨');
+    fs.readFile(htmlfile, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+
+        var result = data.replace(/Document/g, `${projectTitle}`);
+
+        fs.writeFile(htmlfile, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+
+        console.log(`Okkk your working directroy is: ${currentWorkDir}`);
+        console.log('\r\n');
+        console.log(`${style} file has been generated: with RESET ✔`)
+        console.log(`${index} file has been generated: ✔`)
+        console.log(`${script} file has been generated: ✔`)
+        console.log('\r\n');
+        console.log(`${wish}`);
+    });
+
+}
+
+function init() {
+
+    let canRun = false;
+    let options = `${pixidust.FgGreen}yes|${pixidust.resetBCol, pixidust.FgRed}|no${pixidust.resetBCol}`;
+    let setTitle = `${pixidust.FgYellow}${projectTitle}${pixidust.resetBCol}`
+
+    realLine.question(`Do you like to change the project name? (${options}) `, (isYes) => {
+        if (isYes === 'yes') {
+            canRun = true;
+            realLine.question(`What should be the title ? (${setTitle}):  `, function (answer) {
+                if (answer && answer.length) {
+                    projectTitle = answer;
+                }
+                if (canRun) {
+                    asyncCreateFiles(alterConetent)
+                }
+                realLine.close();
+            });
+        }
+        else if (isYes === 'no') {
+            canRun = true;
+            if (canRun) {
+                asyncCreateFiles(alterConetent)
+            }
+            realLine.close();
+        }
+        else {
+            console.log('what the hack');
+            realLine.close();
+        }
+    })
+}
+
+init()
